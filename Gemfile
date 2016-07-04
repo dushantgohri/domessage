03850c32c480d804c696d490637d6eb09d6941cf
dushantgohri <dushantxperia@gmail.com>
2016-07-04 13:15:51 +0000

    finish log-in-log-out

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem	'jquery-rails',									'4.1.1'
 gem	'turbolinks',											'2.5.3'
 gem	'jbuilder',													'2.5.0'
 gem	'sdoc',																	'0.4.1',	group:	:doc
-group	:development,	:test	do	
+group	:development	do	
   gem	'sqlite3',					'1.3.11'	
   gem	'byebug',						'9.0.5'	
   gem	'web-console',	'2.3.0'
@@ -17,7 +17,8 @@ group	:development,	:test	do
 end
 group	:test	do	
     gem	'minitest-reporters',	'1.0.5'
-    gem	'mini_backtrace',					'0.1.3'	
+    gem	'mini_backtrace',					'0.1.3'
+    gem 'guard', '~> 2.14'
     gem	'guard-minitest',					'2.3.1' 
 end
 group	:production	do	
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,16 @@ class UsersController < ApplicationController
   	def	create			
   	 		@user	=	User.new(user_params)		
   	  if @user.save	
-  	      		flash[:success]	=	"Welcome	to	the Domessage!"
+  	      	log_in	@user
+  	      	flash[:success]	=	"Welcome	to	the Domessage!"
   	      	redirect_to	@user
   	  else					
-  	    render	'new'	
+  	        render	'new'	
   	  end	
   	end 
+  	
   	private
   		def	user_params		
-  		  params.require(:user).permit(:name,	:email,	:password, :password_confirmation)	
+  		  params.require( :user).permit(:name ,	:email ,	:password , :password_confirmation)	
   		end 
 end
