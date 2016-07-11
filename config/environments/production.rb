957d0dac02f80355bb45f5e975e0c543567d992a
dushantgohri <dushantxperia@gmail.com>
2016-07-11 11:17:37 +0000

    carrierwave configured

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,16 +67,6 @@ Rails.application.configure do
 	host	=	'domessage.herokuapp.com'	
 	config.action_mailer.default_url_options	=	{	host:	host	}		
 	
-   CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV["S3_ACCESS_KEY"],
-      :aws_secret_access_key  => ENV["S3_SECRET_KEY"],
-      :region                 => 'us-west-2'
-      # Change this for different AWS region. Default is 'us-east-1'
-  }
-  config.fog_directory  = ENV["S3_BUCKET"]
-end
 
 
 	ActionMailer::Base.smtp_settings	=	{ :address	=>	'smtp.sendgrid.net',	
